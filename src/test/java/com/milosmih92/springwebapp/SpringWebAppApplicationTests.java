e3b767c7e8668995995867964566e9f5b1a0d1dc
Milos Mihajlovic <milos.mihajlovic@geta.no>
2021-04-27 23:40:13 +0200

    SPRING-0: Initial project commit.

---
+++ src/test/java/com/milosmih92/springwebapp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milosmih92.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
